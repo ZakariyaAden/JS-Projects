53b30e124479cc1464583751f0836f85cf96df8e
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-08 08:57:30 -0600

    ./Calculator // working on calc logic

--- Projects/calculator/index.js
+++ Projects/calculator/index.js
@@ -1,10 +1,18 @@
-
-
 function takeIndex(id){
     const numButton = document.getElementById(id);
     const calcScreen = document.getElementById("screen");
+    const equals = document.getElementById("calc-button-equals");
     let content = calcScreen.value;
     calcScreen.innerHTML = content + numButton.dataset.index;
+    equals.addEventListener('onclick',e =>{
+        let charSequence = calcScreen.value;
+        let arr = [];
+        for (let i = 0; i < charSequence.length(),i++){
+            arr.push(charSequence.charAt[i]);
+        }
+        arr.forEach(item,index)
+
+    })
 }
 function clear(){
     const calcScreen = document.getElementById("calcInput");
