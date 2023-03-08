50bbdf07fa4fc56f6f27ad2356e55ec9563de928
Zak <114368690+ZakariyaAden@users.noreply.github.com>
2023-03-08 13:35:56 -0600

    ./calculator // working on make pattern attr in js

--- Projects/calculator/index.js
+++ Projects/calculator/index.js
@@ -2,6 +2,11 @@ function takeIndex(id){
     const numButton = document.getElementById(id);
     const calcScreen = document.getElementById("screen");
     const equals = document.getElementById("calc-button-equals");
+
+    let arrOfIndex = [
+        //put index dataset to be iterated over
+    ];
+
     let content = calcScreen.value;
     calcScreen.innerHTML = content + numButton.dataset.index;
     equals.addEventListener('onclick',e =>{
@@ -10,8 +15,11 @@ function takeIndex(id){
         for (let i = 0; i < charSequence.length(),i++){
             arr.push(charSequence.charAt[i]);
         }
-        arr.forEach(item,index)
-
+        arr.forEach(function (item, index) {
+            for(let g = 0; g < arr.length; g++){
+                if(item == )
+            }
+        })
     })
 }
 function clear(){
